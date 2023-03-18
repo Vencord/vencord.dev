26fb67a27a0069961f847e6d77e68fb34d76729a
Vendicated <vendicated@riseup.net>
2023-03-18 18:25:10 +0100

    Add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,17 +2,19 @@ import { defineConfig } from 'astro/config';
 import mdx from "@astrojs/mdx";
 import node from "@astrojs/node";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://vencord.vendicated.dev",
-    vite: {
-        build: {
-            sourcemap: true
-        }
-    },
-    integrations: [mdx()],
-    output: "server",
-    adapter: node({
-        mode: "standalone"
-    })
+  site: "https://vencord.vendicated.dev",
+  vite: {
+    build: {
+      sourcemap: true
+    }
+  },
+  integrations: [mdx(), sitemap()],
+  output: "server",
+  adapter: node({
+    mode: "standalone"
+  })
 });
