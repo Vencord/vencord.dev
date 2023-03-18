78b29d1710e550cb28f6dd5571f04998bd7dc26e
Vendicated <vendicated@riseup.net>
2023-03-18 18:18:47 +0100

    Add /github route

--- src/scripts/constants.ts
+++ src/scripts/constants.ts
@@ -1,4 +1,6 @@
 export const DISCORD_INVITE = "https://discord.gg/D9uwnFnqmd";
+export const GITHUB_URL = "https://github.com/Vendicated/Vencord";
+export const SOURCE_CODE = "https://github.com/Vencord/website";
 
 export const STATUS_MOVED_PERMANENTLY = 301;
 export const STATUS_FOUND = 302;
