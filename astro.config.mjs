e369e117be9c7f85db4bdcdd324eb3297b4decec
Vendicated <vendicated@riseup.net>
2023-03-19 02:55:53 +0100

    Switch to Vercel adapter for now

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,23 +1,27 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
-import node from "@astrojs/node";
 import sitemap from "@astrojs/sitemap";
 import svelte from "@astrojs/svelte";
+import vercel from "@astrojs/vercel/serverless";
+
 const site = "https://vencord.vendicated.dev";
 
 // https://astro.build/config
 export default defineConfig({
-  site,
-  vite: {
-    build: {
-      sourcemap: true
-    }
-  },
-  integrations: [mdx(), sitemap({
-    customPages: [site + "/discord", site + "/github"]
-  }), svelte()],
-  output: "server",
-  adapter: node({
-    mode: "standalone"
-  })
+    site,
+    vite: {
+        build: {
+            sourcemap: true,
+        },
+    },
+    integrations: [
+        mdx(),
+        sitemap({
+            customPages: [site + "/discord", site + "/github"],
+        }),
+        svelte(),
+    ],
+
+    output: "server",
+    adapter: vercel(),
 });
