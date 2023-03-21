7697246edb39a12abef7ec6f116a53976411396c
Vendicated <vendicated@riseup.net>
2023-03-21 22:33:52 +0100

    faq.json: Fix typo & prerender

--- src/pages/faq.json.ts
+++ src/pages/faq.json.ts
@@ -1,12 +1,14 @@
 import { APIRoute } from "astro";
 import { getSortedFaq } from "../scripts/collections";
 
+export const prerender = true;
+
 export const get: APIRoute = async (ctx) => {
     const faq = (await getSortedFaq())
         .map(i => ({
             question: i.data.title,
             // fix [text](/relativeurl) to have full urls
-            answer: i.body.replace(/\[(.+?)\]\((\/.+?\))/g, `[$1](${ctx.url.origin}$2)}`)
+            answer: i.body.replace(/\[(.+?)\]\((\/.+?)\)/g, `[$1](${ctx.url.origin}$2)`)
         }));
 
     return {
