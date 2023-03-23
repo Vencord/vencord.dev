3b2caa8a909b37709939c5b1c1b36179ee2898a9
Lewis Crichton <lewi@lewisakura.moe>
2023-03-23 22:49:18 +0000

    start redesign

---
+++ src/components/typography/index.ts
@@ -0,0 +1,25 @@
+/* headers */
+export { default as Display } from "./Display.astro";
+
+export { default as Headline1 } from "./Headline1.astro";
+export { default as Headline2 } from "./Headline2.astro";
+export { default as Headline3 } from "./Headline3.astro";
+export { default as Headline4 } from "./Headline4.astro";
+export { default as Headline5 } from "./Headline5.astro";
+export { default as Headline6 } from "./Headline6.astro";
+
+/* body */
+export { default as BodyLarge } from "./BodyLarge.astro";
+export { default as BodySmall } from "./BodySmall.astro";
+export { default as BodyExtraSmall } from "./BodyExtraSmall.astro";
+
+/* label */
+export { default as LabelLarge } from "./LabelLarge.astro";
+export { default as LabelMedium } from "./LabelMedium.astro";
+export { default as LabelSmall } from "./LabelSmall.astro";
+export { default as LabelExtraSmall } from "./LabelExtraSmall.astro";
+
+/* overlines */
+export { default as OverlineLarge } from "./OverlineLarge.astro";
+export { default as OverlineMedium } from "./OverlineMedium.astro";
+export { default as OverlineSmall } from "./OverlineSmall.astro";
