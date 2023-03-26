fd890d9a9429804465085bf6660649bc0d800357
Lewis Crichton <lewi@lewisakura.moe>
2023-03-26 16:13:48 +0100

    remove headers from typography and make semantic

--- src/components/typography/index.ts
+++ src/components/typography/index.ts
@@ -3,13 +3,6 @@
 /* headers */
 export { default as Display } from "./Display.astro";
 
-export { default as Headline1 } from "./Headline1.astro";
-export { default as Headline2 } from "./Headline2.astro";
-export { default as Headline3 } from "./Headline3.astro";
-export { default as Headline4 } from "./Headline4.astro";
-export { default as Headline5 } from "./Headline5.astro";
-export { default as Headline6 } from "./Headline6.astro";
-
 /* body */
 export { default as BodyLarge } from "./BodyLarge.astro";
 export { default as BodySmall } from "./BodySmall.astro";
