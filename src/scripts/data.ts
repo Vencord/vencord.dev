3081422e511c5643a6a1fcf4bd6f876173dfa7c7
V <vendicated@riseup.net>
2023-09-09 19:37:56 +0200

    Add plugin READMEs (#27)

--- src/scripts/data.ts
+++ src/scripts/data.ts
@@ -1,10 +1,18 @@
-import { PLUGINS_JSON_URL } from "./constants";
+import { PLUGINS_JSON_URL, PLUGIN_READMES_JSON_URL } from "./constants";
 import { PluginData } from "./types";
 
 export async function fetchPlugins() {
     const res = await fetch(PLUGINS_JSON_URL);
-
     if (!res.ok) throw new Error("Failed to fetch plugins.json: " + res.status);
 
     return res.json() as Promise<PluginData[]>;
+}
+
+export async function fetchPluginReadme(plugin: string) {
+    const res = await fetch(PLUGIN_READMES_JSON_URL);
+    if (!res.ok)
+        throw new Error("Failed to fetch plugin-readmes.json: " + res.status);
+
+    const readmes = await res.json<any>();
+    return readmes[plugin] as string | undefined;
 }
