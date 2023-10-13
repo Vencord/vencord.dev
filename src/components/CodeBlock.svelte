782032b73ae9c1baa41342e0db8d172d238b85a0
Vendicated <vendicated@riseup.net>
2023-10-13 06:03:33 +0200

    forgor accessibility

--- src/components/CodeBlock.svelte
+++ src/components/CodeBlock.svelte
@@ -18,11 +18,13 @@
         <button on:click={copyCode}>
             <svg height="24" viewBox="0 -960 960 960" width="24">
                 {#if !didCopy}
+                    <title>Copy to Clipboard</title>
                     <path
                         fill="currentColor"
                         d="M360-240q-33 0-56.5-23.5T280-320v-480q0-33 23.5-56.5T360-880h360q33 0 56.5 23.5T800-800v480q0 33-23.5 56.5T720-240H360Zm0-80h360v-480H360v480ZM200-80q-33 0-56.5-23.5T120-160v-560h80v560h440v80H200Zm160-240v-480 480Z"
                     />
                 {:else}
+                    <title>Code copied!</title>
                     <path
                         fill="currentColor"
                         d="M382-240 154-468l57-57 171 171 367-367 57 57-424 424Z"
