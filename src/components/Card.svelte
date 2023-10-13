e8124e0fde478a98ae42c85cb67980fe966eb7a0
Vendicated <vendicated@riseup.net>
2023-10-13 06:00:56 +0200

    fix codeblock overflow & add copy button

---
+++ src/components/Card.svelte
@@ -0,0 +1,32 @@
+<script context="module" lang="ts">
+    export const enum CardKind {
+        Warning = "#f0b232",
+        Error = "#ec4144",
+        Success = "#2d7c46",
+        Info = "#5865f2",
+        Blank = "#aaaaaa",
+    }
+</script>
+
+<script lang="ts">
+    export let kind: CardKind;
+
+    const border = kind;
+    const background = kind + Math.floor(0xff / 12).toString(16);
+</script>
+
+<div style="--border:{border};--background:{background}">
+    <slot />
+</div>
+
+<style define:vars={{ border, background }}>
+    div {
+        width: 100%;
+        border: 2px solid var(--border);
+        border-radius: 8px;
+        padding: 0.5rem 1rem;
+        margin: 0.5rem 0;
+        box-sizing: border-box;
+        background-color: var(--background);
+    }
+</style>
