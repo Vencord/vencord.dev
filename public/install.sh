96c3e93068ba06a6c1607f7207f1f503bae02b2c
Vendicated <vendicated@riseup.net>
2025-11-15 18:56:39 +0100

    improve linux install script

--- public/install.sh
+++ public/install.sh
@@ -7,36 +7,37 @@ if [ "$(id -u)" -eq 0 ]; then
 fi
 
 if grep -q "CHROMEOS_RELEASE_NAME" /etc/lsb-release 2>/dev/null; then
-	echo "ChromeOS is not supported! Use the chrome extension. https://chromewebstore.google.com/detail/vencord-web/cbghhgpcnddeihccjmnadmkaejncjndb"
-	exit 1
+    echo "ChromeOS is not supported! Use the chrome extension. https://chromewebstore.google.com/detail/vencord-web/cbghhgpcnddeihccjmnadmkaejncjndb"
+    exit 1
 fi
 
+# Use HOME because some distros have /tmp mounted noexec
 outfile=$(mktemp --tmpdir="$HOME")
 trap 'rm -f "$outfile"' EXIT
 
 echo "Downloading Installer..."
 
-set -- "XDG_CONFIG_HOME=$XDG_CONFIG_HOME"
+set -- "XDG_CONFIG_HOME=$XDG_CONFIG_HOME" "SUDO_USER=$(whoami)"
 
 curl -sS https://github.com/Vendicated/VencordInstaller/releases/latest/download/VencordInstallerCli-Linux \
-  --output "$outfile" \
-  --location \
-  --fail
+    --output "$outfile" \
+    --location \
+    --fail
 
 chmod +x "$outfile"
 
 if command -v sudo >/dev/null; then
-  echo "Running with sudo"
-  sudo env "$@" "$outfile"
+    echo "Running with sudo"
+    sudo env "$@" "$outfile"
 elif command -v doas >/dev/null; then
-  echo "Running with doas"
-  doas env "$@" "$outfile"
+    echo "Running with doas"
+    doas env "$@" "$outfile"
 elif command -v run0 >/dev/null; then
-  echo "Running with run0"
-  run0 env "$@" "$outfile"
+    echo "Running with run0"
+    run0 env "$@" "$outfile"
 elif command -v pkexec >/dev/null; then
-  echo "Running with pkexec"
-  pkexec env "$@" "SUDO_USER=$(whoami)" "$outfile"
+    echo "Running with pkexec"
+    pkexec env "$@" "$outfile"
 else
-  echo "Neither sudo nor doas were found. Please install either of them to proceed."
+    echo "Please install sudo, doas, run0 (systemd), or pkexec (polkit) to continue."
 fi
